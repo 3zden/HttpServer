d2302483d0f1d10b4f0937ae921bfa9ddfae302d
3zden <azeddineabouaam1@gmail.com>
2025-10-14 02:08:15 +0100

    adding json Object

--- src/main/java/com/s3zden/HttpServer/util/json.java
+++ src/main/java/com/s3zden/HttpServer/util/json.java
@@ -1,4 +1,8 @@
 package com.s3zden.HttpServer.util;
 
 public class json {
+    private static objectMapper myObjectMapper ;
+    public static defaultObjectMapper(){
+        objaectMapper om = new objectMapper();
+    }
 }
